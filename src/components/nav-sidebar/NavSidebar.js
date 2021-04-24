3d227fc1b8a6d784764f4bc161d1824cbdf60d7f
Shrikant Dev <shrikant9907@gmail.com>
2021-04-24 21:54:23 +0530

    fixed sidebar ui

--- src/components/nav-sidebar/NavSidebar.js
+++ src/components/nav-sidebar/NavSidebar.js
@@ -3,51 +3,45 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 
 export default class NavSidebar extends Component {
-    render() {
-        return (
-            <div>
-                <div className="card app-sidebar">
-                  <div className="card-body">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                      <NavLink className="navbar-brand" to="/#">
-                        Navbar
-                      </NavLink>
-                      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" />
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarText">
-                              <ul className="navbar-nav mr-auto">
-                                <li className="nav-item">
-                                  <NavLink className="nav-link" to="/#">
-                                    Home
-                                  </NavLink>
-                                </li>
-                                <li className="nav-item">
-                                  <NavLink className="nav-link" to="/#">
-                                    Features
-                                  </NavLink>
-                                </li>
-                                <li className="nav-item">
-                                  <NavLink className="nav-link" to="/#">
-                                    Pricing
-                                  </NavLink>
-                                </li>
-                              </ul>
-                              <NavLink className="btn btn-outline-info my-2 my-sm-0" to="/login">
-                                Login
-                              </NavLink>
-                              <NavLink
-                                className="btn btn-outline-info my-2 my-sm-0 ml-3"
-                                to="/register"
-                              >
-                                Register
-                              </NavLink>
-                            </div>
-                          </nav>
-                  </div>
-                </div>
-                
+  render() {
+    return (
+      <div>
+        <div className="card app-sidebar">
+          <nav className="navbar navbar-expand-lg">
+            <NavLink className="navbar-brand" to="/">
+              IQ-React
+            </NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/home">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/features">
+                    Features
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/pricing">
+                    Pricing
+                  </NavLink>
+                </li>
+              </ul>
+              <NavLink className="btn btn-outline-info" to="/login">
+                Login
+              </NavLink>
+              <NavLink className="btn btn-outline-info" to="/register">
+                Register
+              </NavLink>
             </div>
-        )
-    }
+          </nav>
+        </div>
+      </div>
+    )
+  }
 }
