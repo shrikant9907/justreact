560449854165da421153d9b4eeedb5bf18eaf9c2
Shrikant Dev <shrikant9907@gmail.com>
2021-04-24 19:23:21 +0530

    .env file setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
 
 function App() {
+  const appEnv = process.env.REACT_APP_ENV;
   return (
     <div className="react-app">
         React Project Initial Setup
+        <div className="version">{appEnv}</div>
     </div>
   );
 }
